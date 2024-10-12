f54277d1b8ab4c5b7655ea3eec3a4984f681f5c1
idiarj <idiar16@gmail.com>
2024-10-12 01:24:26 -0400

    tulikitakati

--- frontend protocols/src/CONSTANTS/protocolsINFO.js
+++ frontend protocols/src/CONSTANTS/protocolsINFO.js
@@ -1,10 +1,10 @@
 export const constantsProtocolsINFO = [
     {
         protocolName: 'TCP',
-        description: 'Transmission Control Protocol',
+        description: 'El Protocolo de Control de Transmisión (Transmission Control Protocol en inglés o TCP) es el método de comunicación de datos por defecto entre distintos dispositivos, a través de una red. Este establece y mantiene una conexión entre el emisor y el receptor durante el proceso de transferencia.',
     },
     {
         protocolName: 'UDP',
-        description: 'User Datagram Protocol',
+        description: 'El protocolo de datagramas de usuario (en inglés: User Datagram Protocol o UDP) es un protocolo del nivel de transporte (encapsulado entre la capa de red y la capa de aplicación del modelo OSI) basado en la transmisión sin conexión de datagramas y representa una alternativa al protocolo TCP (Transmission Control Protocol). Ese protocolo permite el envío de datagramas de forma rápida en redes IP sin establecer previamente una conexión, dado que el propio datagrama incorpora suficiente información sobre el destinatario en su cabecera1​.',
     }
 ]
