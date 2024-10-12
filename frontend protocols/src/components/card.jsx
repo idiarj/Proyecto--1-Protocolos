9cc26755231136fc5981c0fef178ccd89798176a
idiarj <idiar16@gmail.com>
2024-10-12 00:57:01 -0400

    el que me stalkee el repo es marico

--- frontend protocols/src/components/card.jsx
+++ frontend protocols/src/components/card.jsx
@@ -1,17 +1,44 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { constantsProtocolsINFO } from '../CONSTANTS/protocolsINFO.js';
 
-export function CardComponent({ title, description, image }) {
+export function CardComponent() {
+  const [selectedProtocol, setSelectedProtocol] = useState('');
+  const [protocolDescription, setProtocolDescription] = useState('');
+
+  const handleChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedProtocol(selectedValue);
+    const selectedProtocolInfo = constantsProtocolsINFO.find(protocol => protocol.protocolName === selectedValue);
+    setProtocolDescription(selectedProtocolInfo ? selectedProtocolInfo.description : '');
+  };
+
   return (
-    <div className="card">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
+    <div>
+      <select onChange={handleChange}>
+        {constantsProtocolsINFO.map((protocol, index) => (
+          <option key={index} value={protocol.protocolName}>
+            {protocol.protocolName}
+          </option>
+        ))}
+      </select>
+      {console.log(selectedProtocol)}
+      <>
+        {selectedProtocol ? (
+          <div>
+            <p>Selected Protocol: {selectedProtocol}</p>
+            <p>Description: {protocolDescription}</p>
+          </div>
+        ) : (
+          <p>Select a protocol</p>
+        )}
+      </>
     </div>
   );
 }
 
 CardComponent.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
 };
